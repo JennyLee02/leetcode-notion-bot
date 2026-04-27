b09da3f2a6a6aca7b8ae2695c9a6b49f655d8021
Jenny Lee <107929895+JennyLee02@users.noreply.github.com>
2026-04-26 20:04:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import requests
 NOTION_API_KEY = os.environ["NOTION_API_KEY"]
 DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
 
-url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
-
 headers = {
     "Authorization": f"Bearer {NOTION_API_KEY}",
     "Notion-Version": "2022-06-28",
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
 }
 
+url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
 response = requests.post(url, headers=headers)
+data = response.json()
 
 print("Status:", response.status_code)
-print("Response:", response.text[:500])
+
+for page in data["results"]:
+    properties = page["properties"]
+    name_property = properties["Name"]["title"]
+
+    if name_property:
+        problem_name = name_property[0]["plain_text"]
+        print("Problem:", problem_name)
