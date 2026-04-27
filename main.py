8550d436f4bd0571ef8bbce8ed780176b301ef39
Jenny Lee <107929895+JennyLee02@users.noreply.github.com>
2026-04-26 20:15:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime
 
 NOTION_API_KEY = os.environ["NOTION_API_KEY"]
 DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
@@ -15,12 +16,23 @@ url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
 response = requests.post(url, headers=headers)
 data = response.json()
 
+today = datetime.utcnow().date()
+
 print("Status:", response.status_code)
+print("\nReview Problems:")
 
 for page in data["results"]:
     properties = page["properties"]
-    name_property = properties["Problem"]["title"]
 
-    if name_property:
-        problem_name = name_property[0]["plain_text"]
-        print("Problem:", problem_name)
+    name_property = properties["Problem"]["title"]
+    if not name_property:
+        continue
+
+    problem_name = name_property[0]["plain_text"]
+    next_review = properties["Next Review"]["date"]
+
+    if next_review:
+        review_date = next_review["start"][:10]
+
+        if review_date <= str(today):
+            print("-", problem_name)
